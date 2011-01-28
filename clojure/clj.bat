4412614eb0f681c048f9a4f39028d14ebe2ed3bb
Benny Tsai <benny.tsai@gmail.com>
2011-01-27 19:30:39 -0700

    removed spurious references to java sql stuff

--- clojure/clj.bat
+++ clojure/clj.bat
@@ -5,6 +5,5 @@ SET VERSION=1.2
 
 SET CLOJURE=%DIR%\clojure-%VERSION%.jar
 SET CONTRIB=%DIR%\clojure-contrib-%VERSION%.jar
-SET SQL_SERVER=%DIR%\sqljdbc4.jar
 
-java -server -cp .;%CLOJURE%;%CONTRIB%;%SQL_SERVER% clojure.main %*
+java -server -cp .;%CLOJURE%;%CONTRIB% clojure.main %*
