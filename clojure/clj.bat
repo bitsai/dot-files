8169c3509b0a5a7f009de4cd260390caca0f27fa
Benny Tsai <benny.tsai@gmail.com>
2011-03-09 16:05:15 -0700

    add Kawa files

--- clojure/clj.bat
+++ clojure/clj.bat
@@ -2,7 +2,6 @@
 
 SET DIR=C:\Progra~1\clojure
 SET VERSION=1.2
-
 SET CLOJURE=%DIR%\clojure-%VERSION%.jar
 SET CONTRIB=%DIR%\clojure-contrib-%VERSION%.jar
 
---
+++ kawa/kawa.bat
@@ -0,0 +1,7 @@
+@echo off
+
+SET DIR=C:\kawa
+SET VERSION=1.11
+SET KAWA=%DIR%\kawa-%VERSION%.jar
+
+java -server -cp .;%KAWA% kawa.repl %*
