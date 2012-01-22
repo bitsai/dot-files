8091f67b104255bc23e12b3bb5de128f040727d2
Benny Tsai <benny.tsai@gmail.com>
2012-01-22 02:49:21 -0700

    update files

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,7 +1,6 @@
 (column-number-mode t)
 (global-hl-line-mode t)
 (setq inhibit-startup-screen t)
-(tool-bar-mode 0)
 
 (setq auto-save-default nil)
 (setq make-backup-files nil)
@@ -11,10 +10,11 @@
 
 (load "~/.emacs.d/clojure-mode/clojure-mode")
 (require 'clojure-mode)
-(setq inferior-lisp-program "~/bin/lein repl")
+(setq inferior-lisp-program "/usr/local/bin/lein repl")
 
 (add-to-list 'load-path "~/.emacs.d/paredit")
 (autoload 'enable-paredit-mode "paredit" t)
-(add-hook 'clojure-mode-hook 'enable-paredit-mode)
 (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+(add-hook 'inferior-lisp-mode-hook 'enable-paredit-mode)
 (add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
+(add-hook 'clojure-mode-hook 'enable-paredit-mode)
--- .profile
+++ .profile
@@ -1,3 +1,4 @@
 export CLICOLOR=1
-export PATH=~/bin:$PATH
 export PS1="\w $ "
+
+alias emacs="/usr/local/Cellar/emacs/23.3b/Emacs.app/Contents/MacOS/Emacs"
