c785fe5bc12683fd587d53a4857cc1b63163a14f
Benny Tsai <benny.tsai@gmail.com>
2012-02-08 19:02:18 -0800

    use charcoal-black color theme, minor clean-up

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,4 @@
 (column-number-mode t)
-(global-hl-line-mode t)
 (setq inhibit-startup-screen t)
 (setq read-file-name-completion-ignore-case t)
 (tool-bar-mode 0)
@@ -10,7 +9,7 @@
 (show-paren-mode t)
 (setq show-paren-style 'expression)
 
-(load "~/.emacs.d/clojure-mode/clojure-mode")
+(add-to-list 'load-path "~/.emacs.d/clojure-mode")
 (require 'clojure-mode)
 (setq inferior-lisp-program "/usr/local/bin/lein repl")
 
@@ -20,3 +19,7 @@
 (add-hook 'inferior-lisp-mode-hook 'enable-paredit-mode)
 (add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
 (add-hook 'clojure-mode-hook 'enable-paredit-mode)
+
+(add-to-list 'load-path "~/.emacs.d/color-theme")
+(require 'color-theme)
+(color-theme-charcoal-black)
