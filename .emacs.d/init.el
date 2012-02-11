ff3e8a8cd536242dbcd800502b64cc0584be087a
Benny Tsai <benny.tsai@gmail.com>
2012-02-11 13:53:21 -0800

    add Emacs speedbar, re-org init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,17 +9,22 @@
 (show-paren-mode t)
 (setq show-paren-style 'expression)
 
+(speedbar 1)
+(setq speedbar-directory-unshown-regexp "^$")
+(speedbar-toggle-show-all-files)
+(speedbar-toggle-updates)
+
 (add-to-list 'load-path "~/.emacs.d/clojure-mode")
 (require 'clojure-mode)
 (setq inferior-lisp-program "/usr/local/bin/lein repl")
 
-(add-to-list 'load-path "~/.emacs.d/paredit")
-(autoload 'enable-paredit-mode "paredit" t)
-(add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
-(add-hook 'inferior-lisp-mode-hook 'enable-paredit-mode)
-(add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
-(add-hook 'clojure-mode-hook 'enable-paredit-mode)
-
 (add-to-list 'load-path "~/.emacs.d/color-theme")
 (require 'color-theme)
 (color-theme-charcoal-black)
+
+(add-to-list 'load-path "~/.emacs.d/paredit")
+(autoload 'enable-paredit-mode "paredit" t)
+(add-hook 'clojure-mode-hook 'enable-paredit-mode)
+(add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+(add-hook 'inferior-lisp-mode-hook 'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
