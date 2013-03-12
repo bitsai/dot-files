431117f8327f651e37bd776db22355ff33825389
Benny Tsai <benny.tsai@gmail.com>
2013-03-12 15:20:45 -0700

    add nrepl, simplify

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,37 +1,28 @@
-;; package manager
+;; packages
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
+(defvar my-packages '(nrepl
+                      paredit))
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
 ;; UI
+(column-number-mode t)
 (load-theme 'wombat t)
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
+(show-paren-mode 1)
 (tool-bar-mode 0)
 
-;; indentation
-(setq js-indent-level 2)
-(setq-default indent-tabs-mode nil)
-
-;; no backups
-(setq auto-save-default nil)
-(setq make-backup-files nil)
-
 ;; paredit
-(add-hook 'clojure-mode-hook (lambda () (paredit-mode t)))
-(add-hook 'emacs-lisp-mode-hook (lambda () (paredit-mode t)))
-(add-hook 'lisp-interaction-mode-hook (lambda () (paredit-mode t)))
-
-;; rebind keys
-(global-set-key "\C-w" 'backward-kill-word)
-(global-set-key "\C-x\C-k" 'kill-region)
-(global-set-key "\C-x\C-m" 'execute-extended-command)
-(global-set-key (kbd "C-x C-M-f") 'find-file-in-project)
+(add-hook 'clojure-mode-hook 'paredit-mode)
+(add-hook 'nrepl-mode-hook 'paredit-mode)
 
 ;; miscellaneous
-(column-number-mode t)
-(ido-mode t)
-(setq inhibit-startup-screen t)
+(setq inhibit-startup-message t)
 (setq read-file-name-completion-ignore-case t)
-(show-paren-mode t)
