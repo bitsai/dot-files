65fc56df24a9e966f732d87c9ce783bd185672ad
Benny Tsai <benny.tsai@gmail.com>
2013-03-20 13:59:56 -0700

    adopt emacs-starter-kit

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,28 +1,18 @@
-;; packages
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
 (defvar my-packages '(nrepl
-                      paredit))
+                      starter-kit
+                      starter-kit-eshell
+                      starter-kit-js
+                      starter-kit-lisp))
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
 
-;; UI
-(column-number-mode t)
-(load-theme 'wombat t)
-(menu-bar-mode 0)
-(scroll-bar-mode 0)
-(show-paren-mode 1)
-(tool-bar-mode 0)
-
-;; paredit
-(add-hook 'clojure-mode-hook 'paredit-mode)
 (add-hook 'nrepl-mode-hook 'paredit-mode)
 
-;; miscellaneous
-(setq inhibit-startup-message t)
-(setq read-file-name-completion-ignore-case t)
+(load-theme 'wombat t)
