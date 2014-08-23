d967a7a8ee227d6b8487a46fac39e30be65dfb22
Benny Tsai <benny.tsai@gmail.com>
2014-08-23 15:06:16 -0700

    update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,12 +5,8 @@
 
 (package-initialize)
 
-(unless package-archive-contents
-  (package-refresh-contents))
-
 (defvar my-packages '(better-defaults
                       cider
-                      ido-ubiquitous
                       paredit
                       smex))
 
@@ -19,11 +15,11 @@
     (package-install p)))
 
 (add-hook 'cider-repl-mode-hook 'paredit-mode)
+(add-hook 'clojure-mode-hook 'cider-turn-on-eldoc-mode)
 (add-hook 'clojure-mode-hook 'paredit-mode)
 (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
+
 (column-number-mode t)
 (global-set-key (kbd "M-x") 'smex)
 (load-theme 'wombat t)
-(setq ido-ubiquitous-mode t)
 (setq inhibit-startup-screen t)
-(setq nrepl-hide-special-buffers t)
