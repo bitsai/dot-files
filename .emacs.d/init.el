8c7386e1693689d2ff12d09d8256bf9fc885fc38
Benny Tsai <benny.tsai@gmail.com>
2016-10-21 21:10:25 -0400

    fix OSX blank square problem

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -33,6 +33,7 @@
 ;; OSX hacks
 (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
 (setq exec-path (append exec-path '("/usr/local/bin")))
+(setq visible-bell nil)
 
 ;; CIDER: dispose of *nrepl-messages* buffers
 (setq nrepl-log-messages nil)
